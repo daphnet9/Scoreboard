58a0f453959d21b36ed5cd3d6c1f27ba9bb0a5c0
daphnet9 <daphnet9@nycstudents.net>
2024-09-30 01:50:18 +0000

    recordPlay method

--- Scoreboard.java
+++ Scoreboard.java
@@ -15,4 +15,22 @@ public class Scoreboard
     {
         return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
     }
-}
+    public void recordPlay(int score)
+    {
+        if (score > 0)
+        {
+            if (activeTeam.equals(teamOne))
+            {
+                teamOneScore += score;
+            }
+            else
+            {
+                teamTwoScore += score;
+            }
+        }
+        else
+        {
+            activeTeam = activeTeam.equals(teamOne) ? teamTwo : teamOne;
+        }
+        }
+    }
